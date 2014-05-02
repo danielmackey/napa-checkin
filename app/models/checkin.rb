52972a12f6e91cec4f140b8f5ecd4e2338814841
Daniel Mackey <daniel@danielmackey.com>
2014-05-02 10:26:07 -0700

    Prevent checkins from happening too frequently

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -5,6 +5,16 @@ class Checkin < ActiveRecord::Base
   belongs_to :business
 
   validates_presence_of :user, :business
+  validate :no_recent_checkins, on: :create
 
   scope :since, -> (time){ where("created_at > ?", time) }
+
+  private
+
+    def no_recent_checkins
+      return unless user && business
+      if Checkin.filter(user: user, business: business).since(5.minutes.ago).any?
+        errors.add(:base, 'already recently checked in')
+      end
+    end
 end
--- spec/models/checkin_spec.rb
+++ spec/models/checkin_spec.rb
@@ -22,6 +22,26 @@ describe Checkin do
       expect(checkin.valid?).to be false
     end
 
+    describe 'recent checkins' do
+
+      it 'is valid if user has already checked more than 5 minute ago' do
+        Timecop.freeze(1.hour.ago) do
+          Checkin.create(user: user, business: business)
+        end
+        new_checkin = Checkin.new(user: user, business: business)
+        expect(new_checkin.valid?).to be true
+      end
+
+      it 'is invalid if user has already checked less than 5 minute ago' do
+        Timecop.freeze(3.minutes.ago) do
+          Checkin.create(user: user, business: business)
+        end
+        new_checkin = Checkin.new(user: user, business: business)
+        expect(new_checkin.valid?).to be false
+      end
+
+    end
+
   end
 
 end
